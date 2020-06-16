37cf36c3d2161882bdc6071f3462f8012cfccdb3
Ubuntu <ubuntu@ip-172-31-30-14.eu-west-1.compute.internal>
2020-06-16 14:43:57 +0000

    Add active learning logic

--- web/app.py
+++ web/app.py
@@ -27,14 +27,15 @@ app.config['CELERY_BROKER_URL'] = 'redis://localhost:6380/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6380/0'
 celery = Celery('app', broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
-q = queue.Queue()
-
+train_queue = queue.Queue()
+unlabelled_queue = queue.Queue()
+sorted_unlabelled_queue = queue.Queue()
 # CELERY TASKS # 
 
 @celery.task
 def stop_training():
-
     stopTrainer.set()
+
 
 
 @celery.task
@@ -82,6 +83,78 @@ def start_training(id, init):
 
     if init:
         trainer.start()
+
+
+class Trainer(Thread):
+
+    def __init__(self, trainable_queue, unlabelled_queue, sorted_unlabelled_queue):
+        Thread.__init__(self)
+        self.train_queue = train_queue
+        self.unlabelled_queue = unlabelled_queue
+        self.sorted_unlabelled_queue = sorted_unlabelled_queue
+
+    def init(self, input_shape, num_classes):
+        
+        self.num_classes = num_classes
+        self.input_shape = input_shape 
+        self.model = self.setup_model()
+        
+    def setup_model(self):
+        baseModel = MobileNetV2(weights="imagenet", include_top=False, input_shape=self.input_shape,
+            input_tensor=layers.Input(shape=self.input_shape))
+
+
+
+        headModel = baseModel.output
+        headModel = layers.AveragePooling2D(pool_size=(3, 3))(headModel)
+        headModel = layers.Flatten(name="flatten")(headModel)
+        headModel = layers.Dense(128, activation="relu")(headModel)
+        headModel = layers.Dropout(0.5)(headModel)
+        headModel = layers.Dense(self.num_classes, activation="softmax")(headModel)
+
+        baseModel.trainable = False
+
+
+        model = keras.Model(inputs=baseModel.input, outputs=headModel)
+        model.compile(loss='binary_crossentropy',
+                    optimizer=keras.optimizers.Adam(),
+                    metrics=['accuracy'])
+        return model
+
+    def SumEntropy(self, pred):
+        def Entropy(prob):
+            return -prob*np.log(prob)
+        Entropy_vect = np.vectorize(Entropy)
+        return np.sum(Entropy_vect(pred))
+
+
+    def MakeQuery(self, unlabelled_set, uncertainty_measure=self.SumEntropy):    
+        '''unlabelled_set : (image, filename) !
+           uncertainty_measure : the higher the more uncertain
+           return dict = {"filename", "score"} decreasingly sorted by score'''
+        dict_keys = ["img_name", "score"]
+        list_of_score_dicts = []
+        for unlabelled_image, filename in unlabelled_set:
+            score_dict = dict.fromkeys(dict_keys)
+            pred = self.model.predict(unlabelled_image)          
+            score_dict["score"].append(uncertainty_measure(pred))
+            score_dict["filename"].append(filename)
+            list_of_score_dicts.append(score_dict)
+        return sorted(list_of_score_dicts, key = lambda i: (i["score"]), reverse=True)
+
+    def run(self):
+        while not stopTrainer.is_set():
+            print("Waiting for feeder to feed us :'( ")
+            train_set = self.train_queue.get()
+            print("We got fed !!\n")
+            for element in train_set:
+                print(element)
+            self.model.fit(train_set, epochs=5)
+            if shouldQuery.is_set():
+                unlabelled_set = self.unlabelled_queue.get()
+                sorted_unlabelled_set = self.MakeQuery(unlabelled_set)
+                shouldQuery.clear()
+                self.sorted_unlabelled_queue.put(sorted_unlabelled_set)
 
 
 # UTILS #
@@ -139,11 +212,12 @@ def feed_data(path):
     
 
     train_set = train_set.batch(4) 
-    
-    q.put(train_set)
+    train_queue.put(train_set)
 
 
-
+@app.route("/query", methods=["POST"])
+def set_query():
+    shouldQuery.set()
 
 @app.route('/train', methods=['POST'])
 def retrieve_data():
@@ -184,8 +258,9 @@ def retrieve_data():
 
 
 if __name__ == '__main__':
-    trainer = Trainer(q)
+    trainer = Trainer(train_queue, unlabelled_queue)
     stopTrainer = Event()
     isTraining = Event()
+    shouldQuery = Event()
     app.run(host='0.0.0.0', port=3333, debug=True)
     
