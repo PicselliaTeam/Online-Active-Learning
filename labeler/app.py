2dbc1190be921a4dc96caa3d4fab82c3bfc23589
BouleJaune <amthierry81@live.fr>
2020-06-19 17:23:43 +0200

    Dash app, communication between the two servers

---
+++ labeler/app.py
@@ -0,0 +1,168 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import flask
+import time
+import os
+from tasks import hello
+import numpy as np
+import json
+import requests 
+from flask import request
+import random
+# import eventlet
+# eventlet.monkey_patch()
+
+server = flask.Flask('app')
+server.config['CELERY_BROKER_URL'] = 'redis://127.0.0.1:6382/0'
+server.config['CELERY_RESULT_BACKEND'] = 'redis://127.0.0.1:6382/0'
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash('app', server=server, external_stylesheets=external_stylesheets)
+
+image_directory  = "assets/images/"
+
+
+class Labeler():
+    def __init__(self, png_dir):
+        self.png_dir = png_dir
+        self.unlabelled = self.configure_dir(png_dir=png_dir)
+        self.images_tosend = []
+        self.ground_truths = []
+        random.shuffle(self.unlabelled)
+        # self.labels_list = self.configure_label()
+        self.labels_list =  ["Morrowind", "Vignes", "xview"]
+        self.update_iter()
+        self.configure_labelmap()
+        
+
+    def configure_dir(self, png_dir):
+        for file in os.listdir(png_dir):
+            if not file.endswith((".png", ".jpg")):
+                print("Your directory does not contain only images, please clean it :)")
+                return
+        print("Scanning {} ... OK".format(png_dir))
+        return os.listdir(png_dir)
+
+    def configure_label(self):
+        Continue = True
+        labels_list = []
+        i=1
+        while Continue:
+            l = input("Label n°{} : ".format(i))
+            if type(l) is str:
+                labels_list.append(l)
+            a = input("Press enter to add a new label or type in something to stop the labelling ")
+            if a != "":
+                Continue=False
+            i+=1
+        return labels_list
+    
+    def configure_labelmap(self):
+        self.labelmap = {}
+        for i, l in enumerate(self.labels_list):
+            self.labelmap[l] = i
+
+    def update_iter(self):
+        # number_tolabel = int(0.1*len(self.to_label+self.unlabelled))
+        # self.to_label = self.unlabelled[:number_tolabel]
+        # [self.unlabelled.remove(p) for p in self.to_label]
+        [self.unlabelled.remove(p) for p in self.images_tosend if p in self.unlabelled]
+        self.iter_images = np.nditer([self.unlabelled])
+        print("iterator updated !")
+
+    def send_data(self):
+        '''reqs = {"labelled_data": [impaths, labels] 
+                "labels_list": self explanatory
+                "unlabelled": [impaths] }'''      
+        path = "./labeler/assets/images"
+        to_keep = self.images_tosend.pop()
+        print("to keep", to_keep)
+        print(len(self.unlabelled))
+        [self.unlabelled.remove(p) for p in self.images_tosend if p in self.unlabelled]
+        print(len(self.unlabelled))
+        to_send_1 = [os.path.join(path, x) for x in self.images_tosend]
+        to_send_2 = [os.path.join(path, x) for x in self.unlabelled]
+        print(self.ground_truths, self.images_tosend)
+        data = {"labelled_data": (to_send_1, self.ground_truths), "labels_list": self.labels_list, "unlabelled": to_send_2}
+        with open("temp.json", "w") as f:
+            json.dump(data, f)
+        r = requests.post("http://localhost:3333/train", data=json.dumps(data))
+        print("data sent!")
+        self.ground_truths = []
+        self.images_tosend = [to_keep]
+        
+labeler = Labeler(png_dir=image_directory)
+
+static_image_route = "/static/"
+center_style = {'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}
+
+app.layout = html.Div(
+    [
+        html.H1('Image to annotate', style=center_style),      
+        html.Div([html.Button(name1, id='sbutton{}'.format(name1), n_clicks=0) for name1 in labeler.labels_list], style=center_style),
+        html.Img(id='image', style=center_style), 
+        html.Div(id="disp_sbutton", children="Labels Button")
+    ]
+)
+ 
+
+
+@server.route("/retrieve_query", methods=['POST'])
+def retrieve_data():
+    '''Retrieve the sorted unlabelled list of dicts of keys [filenames, scores]'''
+    unlabelled_sorted_dict = json.loads(request.data)
+    
+    labeler.unlabelled = [os.path.split(x["filename"])[1] for x in unlabelled_sorted_dict]
+    labeler.update_iter()
+    print("data retrieved!", len(labeler.unlabelled))
+    return "Hey"
+
+
+
+@app.server.route(f'{static_image_route}<image_name>')
+def serve_image(image_name):
+    if image_name not in labeler.unlabelled:
+        raise Exception(f'"{image_name}" is excluded from the allowed static files')
+    return flask.send_from_directory(image_directory, image_name)
+
+@app.server.route("/stop_annotating")
+def serve_stop_image():
+    return flask.send_file("picsell_logo.png")
+
+@app.callback([Output('image', 'src'),
+            Output('disp_sbutton', 'children')],
+     [Input('sbutton{}'.format(name1), 'n_clicks') for name1 in labeler.labels_list])
+def update(*n_clicks):
+    try:
+        image = str(next(labeler.iter_images))
+        labeler.images_tosend.append(image)
+    except StopIteration:
+        return ("/stop_annotating", html.Div(msg))
+
+    msg = "No button was clicked"
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    for label in labeler.labels_list:
+        if label in changed_id:
+            selected_label = label
+            labeler.ground_truths.append(labeler.labelmap[selected_label])
+            msg = f'Label selected was {selected_label}'
+    
+
+    if BATCH_SIZE>len(labeler.ground_truths):
+        print(labeler.ground_truths, labeler.images_tosend)
+        return (static_image_route + image, html.Div(msg))
+
+    else:
+        
+        labeler.send_data()
+        return (static_image_route + image, html.Div(msg))
+    
+BATCH_SIZE = 4
+
+if __name__ == '__main__':
+    # app.run_server(debug=True, use_reloader=False) #to not launch twice everything
+    app.run_server(debug=True, port=3334)
--- trainer/app.py
+++ trainer/app.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import
-from flask import Flask 
+from flask import Flask, request
 import json
-import celery 
 import os
 import requests
-from flask import request
 from celery import Celery
 import sys
 import PIL
@@ -18,35 +16,33 @@ import os
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.applications import MobileNetV2
-from utils import get_diff
+import sys
 
-#TODO: if sys = windows then do this else no, see with docker blablaidk and check imports
-import eventlet
-eventlet.monkey_patch()
+if sys.platform.startswith('win'):
+    import eventlet
+    eventlet.monkey_patch()
 
 
 app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = 'redis://127.0.0.1:6381/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://127.0.0.1:6381/0'
-celery = Celery('app', broker=app.config['CELERY_BROKER_URL'])
-# celery.conf.update(app.config) #TODO: Check with thibault on ubuntu the use of this
+celery_worker = Celery('worker', broker=app.config['CELERY_BROKER_URL'])
 
 # TRAINER CLASS #
 
 class Trainer(Thread):
 
-    def __init__(self, trainable_queue, unlabelled_queue, sorted_unlabelled_queue):
+    def __init__(self, trainable_queue, unlabelled_queue):
         Thread.__init__(self)
         self.train_queue = train_queue
         self.unlabelled_queue = unlabelled_queue
-        self.sorted_unlabelled_queue = sorted_unlabelled_queue
-
+        self.started = False
     def init(self, input_shape, num_classes):
         
         self.num_classes = num_classes
         self.input_shape = input_shape 
         self.model = self.setup_model()
-        
+        self.started = True
     def setup_model(self):
         baseModel = MobileNetV2(weights="imagenet", include_top=False, input_shape=self.input_shape,
             input_tensor=layers.Input(shape=self.input_shape))
@@ -67,26 +63,35 @@ class Trainer(Thread):
         def Entropy(prob):
             return -prob*np.log(prob)
         Entropy_vect = np.vectorize(Entropy)
-        return np.sum(Entropy_vect(pred))
+        return np.sum(Entropy_vect(pred), dtype=np.float64)
 
     def sort_func(self, list_of_score_dicts):
         '''E/E strat, currently only decreasingly sorting by score'''
         return sorted(list_of_score_dicts, key = lambda i: (i["score"]), reverse=True)
+
+    def send_sorted_data(self, data):
+        r = requests.post("http://127.0.0.1:3334/retrieve_query", data=json.dumps(data))
+        with open("error.html", "w") as f:
+            f.write(r.text)
+        with open("dumped.json", "w") as f:
+            json.dump(data, f)
 
     def MakeQuery(self, unlabelled_set, 
                 uncertainty_measure=SumEntropy, EEstrat=sort_func):    
         '''unlabelled_set : (image, filename) !
            uncertainty_measure : the higher the more uncertain
            return dict = {"filename", "score"} decreasingly sorted by score'''
-        dict_keys = ["img_name", "score"]
+        dict_keys = ["filename", "score"]
         list_of_score_dicts = []
-        for unlabelled_image, filename in unlabelled_set:
+        #TODO: Batching !
+        for unlabelled_image, filename in unlabelled_set.as_numpy_iterator():
+            unlabelled_image = np.expand_dims(unlabelled_image, axis=0)
             score_dict = dict.fromkeys(dict_keys)
-            pred = self.model.predict(unlabelled_image)          
-            score_dict["score"].append(uncertainty_measure(pred))
-            score_dict["filename"].append(filename)
+            pred = self.model.predict(unlabelled_image)
+            score_dict["score"] = uncertainty_measure(self, pred[0])
+            score_dict["filename"] = filename.decode("utf-8") 
             list_of_score_dicts.append(score_dict)
-        return EEstrat(list_of_score_dicts)
+        return EEstrat(self, list_of_score_dicts)
 
     def run(self):
         while not stopTrainer.is_set():
@@ -96,68 +101,68 @@ class Trainer(Thread):
             self.model.fit(train_set, epochs=5)
             print("Retrieving the unlabelled set")
             unlabelled_set = self.unlabelled_queue.get()
-            print("Starting predictions ....")
-            sorted_unlabelled_set = self.MakeQuery(unlabelled_set)         
-            self.sorted_unlabelled_queue.put(sorted_unlabelled_set)
-            print("Sending query")
-
-
-
-    
+            print("Making predictions ....")
+            sorted_unlabelled_set = self.MakeQuery(unlabelled_set)  
+            print("Sending query")   
+            self.send_sorted_data(sorted_unlabelled_set) 
+            
+            
 # UTILS #
 
-def decode_img(file_path):
+def decode_img(file_path, model_input_shape=(224, 224)):
     img = tf.io.read_file(file_path)
     img = tf.image.decode_jpeg(img, channels=3) #TODO: Support for grayscale image
     img = tf.image.convert_image_dtype(img, tf.float32)
-    img = tf.image.resize(img, input_shape)
+    img = tf.image.resize(img, model_input_shape)
     return tf.keras.applications.mobilenet_v2.preprocess_input(img)
 
 
-def pre_pro_training(file_path, label):           
-    img = decode_img(file_path)
-    label = tf.one_hot(label, depth=num_classes)
-    return (img, label)
+def training_set_creation(data, model_input_shape, num_classes):
+    print(data)
+    dataset = tf.data.Dataset.from_tensor_slices(tuple(data))     
+    def pre_pro_training(file_path, label): 
+        img = decode_img(file_path, model_input_shape=model_input_shape)
+        label = tf.one_hot(label, depth=num_classes)
+        return (img, label)
+    return dataset.map(pre_pro_training)
 
-def pre_pro_query(file_path): 
-    '''use this later for batching'''
-    img = decode_img(file_path)
-    return (img, file_path)
+def unlabelled_set_creation(data, model_input_shape):
+    def pre_pro_unlabelled(file_path):
+        img = decode_img(file_path, model_input_shape=model_input_shape)
+        return (img, file_path)
+    dataset = tf.data.Dataset.from_tensor_slices(data)
+    return dataset.map(pre_pro_unlabelled)
 
-def feed_training_data(data, num_classes):
-    def dataset_creation(data, model_input_shape, num_classes):
-        dataset = tf.data.Dataset.from_tensor_slices(tuple(data))
-        return dataset.map(pre_pro_training)
 
-    train_set = dataset_creation(data, model_input_shape=(224,224), num_classes=num_classes)
-    train_set = train_set.batch(4) #TODO: Batch size variable 
-    train_queue.put(train_set)
-
-def feed_query_data(data):
-    def dataset_creation(data, model_input_shape):
-        data = tf.data.Dataset.from_tensor_slices(data)
-        return dataset.map(decode_img)
-    unlabelled_set = dataset_creation(data, model_input_shape=(224, 224)) 
-    unlabelled_set.batch(4) #TODO: Variable batch size
-    unlabelled_queue.put(unlabelled_set)
 
 
 # CELERY TASKS # 
 
-@celery.task
+@celery_worker.task(name="stopper")
 def stop_training():
     stopTrainer.set()
 
-@celery.task
-def start_training(data, labels_list, init):
+@celery_worker.task(name="feed_training")
+def feed_training_data(data, labels_list):
     num_classes = len(labels_list)
-    if init:
-        trainer.init(input_shape=(224,224,3), num_classes=num_classes)
-    feed_training_data(data, num_classes)
-    if init:
+    model_input_shape = (224, 224)
+    if not trainer.started:
+        trainer.init(input_shape=model_input_shape+(3,), num_classes=num_classes)
         trainer.start()
-    #TODO: Why two if statements
 
+    train_set = training_set_creation(data, num_classes=num_classes, model_input_shape=model_input_shape)
+    train_set = train_set.batch(4) #TODO: Batch size variable 
+    train_queue.put(train_set)
+    return "feed_training done"
+
+@celery_worker.task(name="feed_query")
+def feed_query_data(data):
+    unlabelled_set = unlabelled_set_creation(data, model_input_shape=(224, 224)) 
+    unlabelled_set.batch(4) #TODO: Variable batch size
+    unlabelled_queue.put(unlabelled_set)
+    return "feed_query done"
+
+# @app.route("/")
 
 @app.route('/train', methods=['POST'])
 def retrieve_data():
@@ -167,33 +172,30 @@ def retrieve_data():
             "labelled_data": [impaths, labels]
             "labels_list": self explanatory
             "init": boolean
-            "unlabelled_set": 
+            "unlabelled": [impaths] 
             }
     """
     data = json.loads(request.data)
 
-    if not os.path.isdir("../assets/annotations"):
-        os.mkdir("../assets/annotations")
+    if not os.path.isdir("./assets/annotations"):
+        os.mkdir("./assets/annotations")
     try:
-        train_nb = len(os.listdir("../assets/annotations/"))
+        train_nb = len(os.listdir("./assets/annotations/"))
     except:
         train_nb = 0
-    path_annot = os.path.join(f"../assets/annotations/annots_{str(train_nb)}.json")
+    path_annot = os.path.join(f"./assets/annotations/annots_{str(train_nb)}.json")
     with open(path_annot, 'w') as f:
         json.dump(data, f)
-    
 
-    start_training.delay(data["labelled_data"], data["labels_list"], data["init"])
-    
-    return 'Hello, World!'
+    feed_training_data.delay(data["labelled_data"], data["labels_list"])
+    feed_query_data.delay(data["unlabelled"])
+    return "hello gary"
 
 
 train_queue = queue.Queue()
 unlabelled_queue = queue.Queue()
-sorted_unlabelled_queue = queue.Queue()
-trainer = Trainer(train_queue, unlabelled_queue, sorted_unlabelled_queue)
+trainer = Trainer(train_queue, unlabelled_queue)
 stopTrainer = Event()
-isTraining = Event()
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3333, debug=True)
